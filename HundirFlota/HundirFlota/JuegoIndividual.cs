b866001195a5b13683f003128296bcc773401fd6
Carmen <carmen.moreno.udaondo@gmail.com>
2023-03-31 10:42:39 +0200

    Actualización: Comentarios clase JuegoIndividual

--- HundirFlota/HundirFlota/JuegoIndividual.cs
+++ HundirFlota/HundirFlota/JuegoIndividual.cs
@@ -19,33 +19,47 @@ namespace HundirFlota
     internal class JuegoIndividual : Partida
     {
         //Atributos
+
         /// <summary>
-        /// Jugador humano iniciado por composición de 
-        /// la clase Jugador
+        /// Instancia de la clase Jugador que permite
+        /// a un jugador humano participar en la partida.
         /// </summary>
         public Jugador jugadorHumano { get; set; }
         /// <summary>
-        /// Jugador automático iniciado por composición de
-        /// la clase Jugaor.
+        /// Instancia de la clase Jugador que permite
+        /// a un jugador automático participar en la partida.
         /// </summary>
         public Jugador jugadorAuto { get; set; }
 
         //Constructores
-        /// <summary>
-        /// Constructor parametrizado de la clase JuegoInividual
-        /// </summary>
-        public JuegoIndividual() { }
 
         /// <summary>
-        /// Constructor parametrizado de la clase JuegoIndividual
+        /// Constructor vacío de la clase JuegoIndividual. Inicializa
+        /// las instancias de la clase Jugador.
         /// </summary>
-        /// <param name="jugadorHumano"></param>
-        /// <param name="jugadorAuto"></param>
-        public JuegoIndividual(Jugador jugadorHumano, Jugador jugadorAuto)
+        public JuegoIndividual() 
         {
-            this.jugadorHumano = jugadorHumano;
-            this.jugadorAuto = jugadorAuto;
+            jugadorHumano = new Jugador();
+            jugadorAuto = new Jugador();
         }
+
+        /// <summary>
+        /// Constructor parametrizado de la clase JuegoIndividual.
+        /// Inicializa las instancias de la clase Jugador.
+        /// </summary>
+        /// <param name="jugadorHumano">
+        /// Instancia de Jugador que representa al jugador Humano.
+        /// </param>
+        /// <param name="jugadorAuto">
+        /// Instancia de Jguador que repsenta al jugador automático
+        /// </param>
+        public JuegoIndividual(Jugador _jugadorHumano, Jugador _jugadorAuto)
+        {
+            jugadorHumano = _jugadorHumano;
+            jugadorAuto = _jugadorAuto;
+        }
+
+        // Métodos
     }
 
 }
